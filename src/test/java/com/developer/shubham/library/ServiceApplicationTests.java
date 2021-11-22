f9d1030d87de79ccbc2c32671ac2c99d492a29f4
DeveloperShubham1997 <shubham.ruvasiya@gmail.com>
2021-11-22 20:49:21 +0530

    initial commit

---
+++ src/test/java/com/developer/shubham/library/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.shubham.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
